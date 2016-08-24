17ed460039cec043839c8ff43eb4167be84331ed
Filipp.Stankevich <filipp.stankevich@thomsonreuters.com>
2016-08-24 14:53:09 +0300

    Refactoring of PrinterFactory

--- src/main/java/com/santaclaus/presents/main/PrinterFactory.java
+++ src/main/java/com/santaclaus/presents/main/PrinterFactory.java
@@ -5,7 +5,6 @@ import com.santaclaus.presents.view.FilePresentPrinter;
 import com.santaclaus.presents.view.MutiplePresentPrinter;
 import com.santaclaus.presents.view.PresentPrinter;
 
-
 public class PrinterFactory {
     
     private final static String CONSOLE_PRINTER = "console";
@@ -18,21 +17,37 @@ public class PrinterFactory {
     }
     
     public PresentPrinter create(String ... printerTypes){
-        
-        MutiplePresentPrinter multiplePrinter = new MutiplePresentPrinter();
-        
-        for (String printerType : printerTypes){
-            if (printerType.equals(CONSOLE_PRINTER)){
-                PresentPrinter consolePrinter = new ConsolePresentPrinter();
-                multiplePrinter.addPrinter(consolePrinter);                
-            }
-            if (printerType.equals(FILE_PRINTER)){
-                PresentPrinter filePrinter = new FilePresentPrinter(filePath);
-                multiplePrinter.addPrinter(filePrinter);
-            }
+
+        if (printerTypes.length == 0) {
+            throw new IllegalArgumentException("Must specify non-empty array of printer types");
         }
-        
-        return multiplePrinter;
+
+        PresentPrinter printer;
+        if (printerTypes.length == 1) {
+            String type = printerTypes[0];
+            printer = createPrinter(type);
+        } else {
+            MutiplePresentPrinter multiplePrinter = new MutiplePresentPrinter();
+            for (String printerType : printerTypes){
+                PresentPrinter childPrinter = createPrinter(printerType);
+                multiplePrinter.addPrinter(childPrinter);
+
+            }
+            printer = multiplePrinter;
+        }
+
+        return printer;
+    }
+
+    private PresentPrinter createPrinter(String type) {
+        switch (type) {
+            case CONSOLE_PRINTER:
+                return new ConsolePresentPrinter();
+            case FILE_PRINTER:
+               return new FilePresentPrinter(filePath);
+            default:
+                throw new IllegalArgumentException("Unknown printer type");
+        }
     }
     
 }
